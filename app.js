756e42736c41b43f38fd3479bbad61b2808be641
anthony <atran.dummy@gmail.com>
2013-06-10 19:34:52 +0200

    save the file to temporary folder; convert it to a wav file

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 
 /**
- * Module dependencies.
- */
+* Module dependencies.
+*/
 
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server)
+  , fs = require('fs')
+  , ffmpeg = require('fluent-ffmpeg');
 
-server.listen(8080);
+server.listen(3000);
 
 // config
 app.configure(function(){
@@ -28,7 +30,26 @@ app.get('/', function(req, res) {
 app.post('/api/videos', function(req, res) {
   console.log(req.body)
   console.log(JSON.stringify(req.files));
+  
+  var tmp_path = req.files.video_file.path;
+  var target_path = 'tmp/' + req.files.video_file.name;
+
+  fs.rename(tmp_path, target_path, function(err) {
+    fs.unlink(tmp_path, function(err){ 
+      convert_to_audio(target_path);
+    });
+  })
+  
 })
+
+var convert_to_audio = function(path) {
+  var proc = new ffmpeg({ source: path })
+  .toFormat('wav')
+  .saveToFile('public/audio/xx.wav', function(stdout, stderr){
+    console.log('saved as wav');
+  });
+
+}
 
 // socket
 io.sockets.on('connection', function(socket) {
