7c51a63aad0aa4b9894c2c4d375bab5dc0f4f7d4
anthony <atran.dummy@gmail.com>
2013-06-10 17:46:23 +0200

    implement progress loading on upload

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
   , port = 3000;
@@ -22,26 +20,93 @@ var filter = function(pattern, fun){
 }
 watch('./', filter(/\.jade|\.js|\.css$/i, function() {
   io.sockets.emit('reload') //send a message to all clients
-}); );
+}))
 
 // all environments
 var io = require('socket.io').listen(app.listen(port));app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// routes 
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/api/videos', function(req, res) {
+  console.log(JSON.stringify(req));
+})
 
+
+var files = {};
+
+socket.on('start', function(data) {
+  var name = data['name'];
+  files[name] = {
+    filesize   : data['size'],
+    data       : "",
+    downloaded : 0
+  }
+  var place = 0;
+  try {
+    var stat = fs.statSync('temp/' + name);
+    if (stat.isFile()) {
+      files[name]['downloaded'] = stat.size
+      place = stat.size / 524288
+    }
+  } catch (er){}
+
+  fs.open('temp/' + name, 'a', 0755, function(err, fd){
+    if (err) {
+      console.log(err)
+    } 
+    else {
+      files[name]['handler'] = fd;
+      socket.emit('moreData',{
+        place  : place,
+        percent: 0,
+      });
+    }
+  });
+
+});
+
+socket.on('upload', function(data) {
+  var name = data['name'];
+  files[name]['downloaded'] += data['data'].length;
+  files[name]['data'] += data['data'];
+
+  // fully downloaded
+  if (files[name]['downloaded'] == files[name]['filesize']) {
+    fs.write(files[name]['handler'], files[name]['data'], null, 'Binary')
+  }
+  // if data buffer > 10 MB
+  else if (files[name]['data'].length > 10485760) {
+    fs.write(files[name]['handler'], files[name]['data'], null, 'Binary', function(err, written){
+      files[name]['data'] = "";
+      var place = files[name]['downloaded'] / 524288;
+      var percent = (files[name]['downloaded'] / files[name]['filesize']) * 100;
+      socket.emit('moreData', {
+        place: place,
+        percent: percent
+      })
+    })
+  }
+  else {
+    var place = files[name]['downloaded'] / 524288;
+    var percent = (files[name]['downloaded'] / files[name]['filesize']) * 100;
+    socket.emit('moreData', {
+      place: place,
+      percent: percent
+    })
+  }
+  
+})
+
+// Start server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- public/coffee/uploader.coffee
+++ public/coffee/uploader.coffee
@@ -1,5 +1,29 @@
 $ ->
-
+  selected_file = null
+  lat = ""
+  lng = ""
+  name = ""
   $('.video-upload').on 'change', (e) ->
-    oFile = document.getElementById('image_file').files[0]
-    
+    selected_file = e.target.files[0]
+    lat  = $('.lat').val()
+    lng  = $('.lng').val()
+    name = $('.name').val()
+
+  $('#upload-me').on 'click', (e) ->
+    FReader = new FileReader()
+    FReader.onload = (e) ->
+      socket.emit('upload',
+        'name'  : name
+        'lat'   : lat
+        'lng'   : lng
+        'data'  : e.target.result
+      )
+
+    socket.emit('start',
+      'name'  : name
+      'lat'   : lat
+      'lng'   : lng
+      'data'  : selected_file.size
+    )
+
+
