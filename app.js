2ff1ce5b266c822548ba8da65dc37c9da85ef55b
anthony <atran.dummy@gmail.com>
2013-06-10 18:18:46 +0200

    cleaning up a bit

--- app.js
+++ app.js
@@ -3,33 +3,22 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , port = 3000;
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 
-var app = express();
+server.listen(8080);
 
-// livereload
-var watch = require('node-watch');
-
-var filter = function(pattern, fun){
-  return function(filename){
-    if(pattern.test(filename)){fun(filename);}
-  }
-}
-watch('./', filter(/\.jade|\.js|\.css$/i, function() {
-  io.sockets.emit('reload') //send a message to all clients
-}))
-
-// all environments
-var io = require('socket.io').listen(app.listen(port));app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(__dirname + '/public'));
+// config
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  io.set('log level', 1);
+});
 
 // routes 
 app.get('/', function(req, res) {
@@ -37,76 +26,12 @@ app.get('/', function(req, res) {
 });
 
 app.post('/api/videos', function(req, res) {
-  console.log(JSON.stringify(req));
+  console.log(req.body)
+  console.log(JSON.stringify(req.files));
 })
 
-
-var files = {};
-
-socket.on('start', function(data) {
-  var name = data['name'];
-  files[name] = {
-    filesize   : data['size'],
-    data       : "",
-    downloaded : 0
-  }
-  var place = 0;
-  try {
-    var stat = fs.statSync('temp/' + name);
-    if (stat.isFile()) {
-      files[name]['downloaded'] = stat.size
-      place = stat.size / 524288
-    }
-  } catch (er){}
-
-  fs.open('temp/' + name, 'a', 0755, function(err, fd){
-    if (err) {
-      console.log(err)
-    } 
-    else {
-      files[name]['handler'] = fd;
-      socket.emit('moreData',{
-        place  : place,
-        percent: 0,
-      });
-    }
-  });
+// socket
+io.sockets.on('connection', function(socket) {
 
 });
 
-socket.on('upload', function(data) {
-  var name = data['name'];
-  files[name]['downloaded'] += data['data'].length;
-  files[name]['data'] += data['data'];
-
-  // fully downloaded
-  if (files[name]['downloaded'] == files[name]['filesize']) {
-    fs.write(files[name]['handler'], files[name]['data'], null, 'Binary')
-  }
-  // if data buffer > 10 MB
-  else if (files[name]['data'].length > 10485760) {
-    fs.write(files[name]['handler'], files[name]['data'], null, 'Binary', function(err, written){
-      files[name]['data'] = "";
-      var place = files[name]['downloaded'] / 524288;
-      var percent = (files[name]['downloaded'] / files[name]['filesize']) * 100;
-      socket.emit('moreData', {
-        place: place,
-        percent: percent
-      })
-    })
-  }
-  else {
-    var place = files[name]['downloaded'] / 524288;
-    var percent = (files[name]['downloaded'] / files[name]['filesize']) * 100;
-    socket.emit('moreData', {
-      place: place,
-      percent: percent
-    })
-  }
-  
-})
-
-// Start server
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
--- public/coffee/uploader.coffee
+++ public/coffee/uploader.coffee
@@ -1,29 +1,4 @@
 $ ->
-  selected_file = null
-  lat = ""
-  lng = ""
-  name = ""
-  $('.video-upload').on 'change', (e) ->
-    selected_file = e.target.files[0]
-    lat  = $('.lat').val()
-    lng  = $('.lng').val()
-    name = $('.name').val()
 
-  $('#upload-me').on 'click', (e) ->
-    FReader = new FileReader()
-    FReader.onload = (e) ->
-      socket.emit('upload',
-        'name'  : name
-        'lat'   : lat
-        'lng'   : lng
-        'data'  : e.target.result
-      )
-
-    socket.emit('start',
-      'name'  : name
-      'lat'   : lat
-      'lng'   : lng
-      'data'  : selected_file.size
-    )
 
 
